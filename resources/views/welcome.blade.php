88dba2b22f31e6ef1f60161cd65423426fec43f1
Brain Wonderson <112601710+brainwonderson@users.noreply.github.com>
2024-10-18 13:09:15 +0700

    terbaru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
                 <div class="flex space-x-4 overflow-x-auto">
                     <!-- Materi 1 -->
                     <div class="bg-white p-4 border border-gray-300 shadow-sm">
-                        <h3 class="font-medium">Materi 1</h3>
+                        <h3 class="font-medium">Software Development <br>LifeCycle</br></h3>
                         <ul class="list-none p-0 m-0">
                             <li class="mb-2"><a href="https://www.youtube.com/watch?v=video5"
                                     class="text-blue-600 hover:text-blue-800">Video 5</a></li>

3adf2a9fa9414507adf882d0e89c1d52de2dc54d
Brain Wonderson <112601710+brainwonderson@users.noreply.github.com>
2024-10-18 15:37:58 +0700

    cookie

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CookieController;  // Tambahkan ini untuk mengimpor CookieController
 
 Route::get('/', function () {
     return view('welcome');
-}) -> name('welcome');
+})->name('welcome');
 
 Route::get('/login', function () {
     return view('login');
-}) -> name('login');
+})->name('login');
 
 Route::get('/test', function () {
     return view('test');
-}) -> name('test');
+})->name('test');
 
 Route::get('/home', function () {
     return view('homepage');
-}) -> name('home');
-
+})->name('home');
 
 Route::get('/materi', function () {
     return view('materi');
-}) -> name('materi');
+})->name('materi');
 
-Route::post('/login', [AuthController::class, 'loginPost'])->name('login.post'); 
+Route::post('/login', [AuthController::class, 'loginPost'])->name('login.post');
+
+// Rute untuk CookieController
+Route::get('/set-cookie', [CookieController::class, 'setCookie']);
+Route::get('/get-cookie', [CookieController::class, 'getCookie']);  // Diperbaiki 'geetCookie' menjadi 'getCookie'
+Route::get('/delete-cookie', [CookieController::class, 'deleteCookie']);
